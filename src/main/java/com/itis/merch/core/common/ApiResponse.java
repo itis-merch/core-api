02ada11533bb706efca7936a656c0b682243d899
vitterre <bagatovbb@gmail.com>
2023-03-21 22:11:26 +0300

    feat: add api response class for future controllers and custom exceptions

---
+++ src/main/java/com/itis/merch/core/common/ApiResponse.java
@@ -0,0 +1,40 @@
+package com.itis.merch.core.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * We use this class in order to standardize the API response that
+ * the server returns when the client requests.
+ * <p>
+ * The {@code ApiResponse} class can be used, for example, in
+ * controllers to form a response to a client calling a specific
+ * endpoint. For instance, if a client requests a resource that
+ * does not exist on the server, the controller can return an {@code
+ * ApiResponse} object with the code {@code 404} and a message
+ * indicating that the requested resource was not found.
+ * <p>
+ * Additionally, the {@code ApiResponse} class can be used in
+ * exception handlers that may occur during request processing.
+ * In this case, the {@code ApiResponse} class can contain an error
+ * code and a message describing the error that will be returned to
+ * the client in response to the request.
+ */
+@AllArgsConstructor
+public final class ApiResponse {
+
+	/*----- Private fields -----*/
+
+	/**
+	 * HTTP code that server returns in response to a request from
+	 * the client.
+	 */
+	@Getter private final Integer code;
+
+	/**
+	 * Additional response status information that can be useful for
+	 * the client.
+	 */
+	@Getter private final String message;
+
+}
