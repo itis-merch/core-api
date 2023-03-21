590fc351e719ed0fffc3d38d285eec9a5acca5ec
vitterre <bagatovbb@gmail.com>
2023-03-21 21:54:53 +0300

    feat(config): add web config for cors mapping

---
+++ src/main/java/com/itis/merch/core/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.itis.merch.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * This class is responsible for configuring CORS (Cross-Origin
+ * Resource Sharing) in a Spring application.
+ * <p>
+ * {@code @EnableWebMvc} tells Spring that we want to configure
+ * the web application using {@code WebMvcConfigurer}.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	/**
+	 * {@code addCorsMappings()} is a method of {@code WebMvcConfigurer}
+	 * that allows us to add CORS settings to the application.
+	 * <p>
+	 * We add CORS configuration for all URLs starting with {@code
+	 * /api/v1/}, allowing only {@code GET} and {@code POST} methods, an
+	 * any domain with {@code allowedOrigins("**")}. This way, the
+	 * application can handle requests from any domain that uses these methods.
+	 *
+	 * @param registry
+	 */
+	@Override
+	public void addCorsMappings(final CorsRegistry registry) {
+		registry.addMapping("/api/v1/**")
+						.allowedOrigins("**")
+						.allowedMethods("GET", "POST");
+	}
+}
