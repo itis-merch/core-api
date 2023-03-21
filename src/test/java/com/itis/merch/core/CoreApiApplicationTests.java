70a6b0426702566de016e85a29a0c2427bd1b690
vitterre <bagatovbb@gmail.com>
2023-03-21 19:20:21 +0300

    build(core): init spring project

---
+++ src/test/java/com/itis/merch/core/CoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itis.merch.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
