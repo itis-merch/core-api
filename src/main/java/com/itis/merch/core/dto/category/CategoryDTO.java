7f7555c62456edb9d12ae60b2fcf804f421282eb
vitterre <bagatovbb@gmail.com>
2023-03-22 17:39:38 +0300

    feat(models): add dto for each existing model

---
+++ src/main/java/com/itis/merch/core/dto/category/CategoryDTO.java
@@ -0,0 +1,80 @@
+package com.itis.merch.core.dto.category;
+
+import com.itis.merch.core.models.Category;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * The {@code CategoryDTO} class represents a data transfer object for
+ * a category entity. It is used to transfer category data between
+ * different layers of an application or across a network.
+ * <p>
+ * This class contains fields that correspond to the fields of the {@link
+ * Category} entity class.
+ * <p>
+ * Also, this class is annotated with {@code @Data} which generates all
+ * the boilerplate code required for a data class such as getters, setters,
+ * equals, hashCode, and toString methods.
+ * <p>
+ * Each CategoryDTO has a name and description.
+ * NOTE: need to add {@code getCategoryFromDTO} in the future {@code
+ * CategoryService} later.
+ */
+@Data
+@NoArgsConstructor
+public final class CategoryDTO {
+
+	/*----- Private fields -----*/
+
+	/**
+	 * The id of the category.
+	 */
+	private Integer id;
+
+	/**
+	 * The name of the category.
+	 */
+	@NotBlank
+	@NotNull
+	private String name;
+
+	/**
+	 * The description of the category.
+	 */
+	@NotBlank
+	@NotNull
+	private String description;
+
+
+	/*----- Constructors -----*/
+
+	/**
+	 * Constructs a new {@code CategoryDTO} object with the specified
+	 * parameters.
+	 *
+	 * @param id          the ID of the category
+	 * @param name        the name of the category (must not be blank or null)
+	 * @param description the description of the category (must not be blank or null)
+	 */
+	public CategoryDTO(final Integer id, @NotBlank @NotNull final String name, @NotBlank @NotNull final String description) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	/**
+	 * Constructs a new {@code CategoryDTO} object using a {@link Category}
+	 * entity object.
+	 *
+	 * @param category the {@link Category} object to convert to a DTO object
+	 */
+	public CategoryDTO(@NotNull final Category category) {
+		this.setId(category.getId());
+		this.setName(category.getName());
+		this.setDescription(category.getDescription());
+	}
+
+}
