25f08c33859e8180b68c8126cb9b88504a4fd39c
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-03-30 20:45:36 +0300

    docs(ControllerAdvice): update documantations for method (catchException)

--- src/main/java/com/itis/merch/core/controllers/ControllerAdvice.java
+++ src/main/java/com/itis/merch/core/controllers/ControllerAdvice.java
@@ -1,5 +1,8 @@
 /**
- * The ControllerAdvice class provides global exception handling for all controllers in the system.
+
+ The ControllerAdvice class provides global exception handling for all controllers in the system.
+ It is annotated with @ControllerAdvice to indicate that it is a global exception handler for all controllers.
+ The class contains one method, catchException, that handles runtime exceptions and returns an ApiResponse object containing an error message.
  */
 package com.itis.merch.core.controllers;
 
@@ -8,16 +11,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * The ControllerAdvice class provides global exception handling for all controllers in the system.
+
+ The ControllerAdvice class provides global exception handling for all controllers in the system.
+
+ It is annotated with @ControllerAdvice to indicate that it is a global exception handler for all controllers.
  */
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
 
 	/**
-	 * Handles runtime exceptions that occur in any controller in the system and returns an ApiResponse object containing an error message.
-	 *
-	 * @param exception The RuntimeException that occurred.
-	 * @return An ApiResponse object containing a false status and the error message.
+	 Handles runtime exceptions that occur in any controller in the system and returns an ApiResponse object containing an error message.
+	 @param exception The RuntimeException that occurred and needs to be caught.
+	 @return An ApiResponse object containing a false status and the error message of the exception.
+	 The method is annotated with @ExceptionHandler to indicate that it handles exceptions of the specified type.
+	 The method is also annotated with @ResponseBody to indicate that it returns a response body that will be written directly to the response.
 	 */
 	@ResponseBody
 	@ExceptionHandler(Exception.class)
