c2630b0b1da375237127e147484b9aec9fff62e6
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-03-30 03:47:41 +0300

    docs: add documentations for product contoller

--- src/main/java/com/itis/merch/core/controllers/ProductController.java
+++ src/main/java/com/itis/merch/core/controllers/ProductController.java
@@ -1,3 +1,6 @@
+/**
+ * The ProductController class handles incoming HTTP requests and returns HTTP responses for product-related operations.
+ */
 package com.itis.merch.core.controllers;
 
 import com.itis.merch.core.dto.product.ProductDTO;
@@ -29,16 +32,34 @@ public class ProductController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(productService.addNewProduct(productDTO));
 	}
 
+	/**
+	 * Retrieves a list of all products from the database and returns it with HTTP status 200 OK.
+	 *
+	 * @return A ResponseEntity containing the list of all products and HTTP status 200 OK.
+	 */
 	@GetMapping
 	public ResponseEntity<List<ProductDTO>> getProducts() {
 		return ResponseEntity.ok(productService.getAll());
 	}
 
+	/**
+	 * Retrieves a product with a specific ID from the database and returns it with HTTP status 200 OK.
+	 *
+	 * @param productId The ID of the product to be retrieved.
+	 * @return A ResponseEntity containing the retrieved ProductDTO object and HTTP status 200 OK.
+	 */
 	@GetMapping("/{product-id}")
 	public ResponseEntity<ProductDTO> getProductById(@PathVariable("product-id") Integer productId) {
 		return ResponseEntity.ok(productService.getById(productId));
 	}
 
+	/**
+	 * Updates a product with a specific ID in the database and returns the updated product with HTTP status 200 OK.
+	 *
+	 * @param productId  The ID of the product to be updated.
+	 * @param productDTO The ProductDTO object representing the updated product.
+	 * @return A ResponseEntity containing the updated ProductDTO object and HTTP status 200 OK.
+	 */
 	@PostMapping("/{product-id}")
 	public ResponseEntity<ProductDTO> updateProductById(@PathVariable("product-id") Integer productId, @RequestBody ProductDTO productDTO) {
 		return ResponseEntity.ok(productService.updateById(productDTO, productId));
