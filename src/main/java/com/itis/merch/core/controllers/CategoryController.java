57af198ccbc38787fcdc967b96eb4b6da15fbf7b
Marat <iimarioii3@mail.ru>
2023-03-31 16:33:28 +0300

    feat: add getCategories to CategoryController

--- src/main/java/com/itis/merch/core/controllers/CategoryController.java
+++ src/main/java/com/itis/merch/core/controllers/CategoryController.java
@@ -1,5 +1,9 @@
 package com.itis.merch.core.controllers;
 
+import com.itis.merch.core.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/categories")
 public class CategoryController {
 
+	@Autowired
+	CategoryService categoryService;
+
+	@GetMapping
+	public ResponseEntity getCategories() {
+		try{
+			return ResponseEntity.ok(categoryService.getAll);
+		}
+		catch (Exception e){
+			return ResponseEntity.badRequest().body("Error!");
+		}
+	}
 }
