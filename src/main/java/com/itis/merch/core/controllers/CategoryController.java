1993cded4e1d330c8231cffd0da928f0b607ab8e
Marat <iimarioii3@mail.ru>
2023-03-31 17:49:22 +0300

    chore:trivial code changes

--- src/main/java/com/itis/merch/core/controllers/CategoryController.java
+++ src/main/java/com/itis/merch/core/controllers/CategoryController.java
@@ -30,8 +30,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.criteria.CriteriaBuilder;
-
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
