fa3eab61fa987390a8cb94e04035eca822d279c3
Marat <iimarioii3@mail.ru>
2023-03-31 16:57:35 +0300

    feat: add findByName to CategoryRepository

--- src/main/java/com/itis/merch/core/repositories/CategoryRepository.java
+++ src/main/java/com/itis/merch/core/repositories/CategoryRepository.java
@@ -2,6 +2,13 @@
  * The CategoryRepository interface is responsible for managing the persistence of Category objects in the database.
  * This interface extends the JpaRepository interface, which provides basic CRUD operations for Category entities.
  * It also specifies that the primary key for Category entities is an Integer.
+ *
+ * In addition to the basic CRUD operations, this interface defines a custom method, findByName(), which takes a String
+ * parameter representing the name of the category to search for. This method returns a Category object that matches the
+ * specified name.
+ *
+ * Note that this method does not require an implementation, as Spring Data JPA will automatically generate the necessary
+ * SQL query based on the method name.
  */
 
 package com.itis.merch.core.repositories;
@@ -10,5 +17,5 @@ import com.itis.merch.core.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
+	Category findByName(String name);
 }
