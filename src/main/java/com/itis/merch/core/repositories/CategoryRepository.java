5a4368b40f9758b733d9459485dc31983958168d
Marat <iimarioii3@mail.ru>
2023-03-31 16:20:24 +0300

    feat(doc): add documentation to CategoryRepository

--- src/main/java/com/itis/merch/core/repositories/CategoryRepository.java
+++ src/main/java/com/itis/merch/core/repositories/CategoryRepository.java
@@ -1,3 +1,9 @@
+/**
+ * The CategoryRepository interface is responsible for managing the persistence of Category objects in the database.
+ * This interface extends the JpaRepository interface, which provides basic CRUD operations for Category entities.
+ * It also specifies that the primary key for Category entities is an Integer.
+ */
+
 package com.itis.merch.core.repositories;
 
 import com.itis.merch.core.models.Category;
