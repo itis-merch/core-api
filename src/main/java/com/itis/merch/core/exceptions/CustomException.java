bd3d9b734cf6e3efbb16df91f64e03d3ac80493b
vitterre <bagatovbb@gmail.com>
2023-04-01 13:46:22 +0300

    docs: add docs for CustomException

--- src/main/java/com/itis/merch/core/exceptions/CustomException.java
+++ src/main/java/com/itis/merch/core/exceptions/CustomException.java
@@ -3,12 +3,32 @@ package com.itis.merch.core.exceptions;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+/**
+ * This class represents a custom exception with an associated HTTP status code.
+ */
 public class CustomException extends Exception {
+
+	/*----- Private fields -----*/
+
+	/**
+	 * The HTTP status code associated with the exception.
+	 */
 	@Getter
 	private final HttpStatus httpStatus;
 
+
+	/*----- Constructors -----*/
+
+	/**
+	 * Constructs a new CustomException object with the given message and HTTP status
+	 * code.
+	 *
+	 * @param message    the message to associate with the exception.
+	 * @param httpStatus the HTTP status code to associate with the exception.
+	 */
 	public CustomException(String message, HttpStatus httpStatus) {
 		super(message);
 		this.httpStatus = httpStatus;
 	}
+
 }
