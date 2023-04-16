9be348b6671272e1c962aebc3bf1f65777711d5e
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-04-16 20:06:17 +0300

    docs: add documentations for WebSecurityConfiguration

--- src/main/java/com/itis/merch/core/security/config/WebSecurityConfiguration.java
+++ src/main/java/com/itis/merch/core/security/config/WebSecurityConfiguration.java
@@ -1,3 +1,7 @@
+/**
+ * The WebSecurityConfiguration class is a configuration class that enables and configures web security for the application.
+ * It extends the WebSecurityConfigurerAdapter class provided by Spring Security.
+ */
 package com.itis.merch.core.security.config;
 
 import org.springframework.context.annotation.Bean;
@@ -10,20 +14,31 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	/**
+	 * This method creates and returns an AuthenticationManager bean that will be used by the application.
+	 * @return the authentication manager bean
+	 */
 	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
 
+	/**
+	 * This method configures the HttpSecurity object to specify security constraints for HTTP requests.
+	 * The "/auth/**" endpoint is permitted for all requests while all other requests are authenticated.
+	 * The "/auth/logout" endpoint is used to log out the user, and the user's session is invalidated, the authentication is cleared,
+	 * and the JSESSIONID cookie is deleted.
+	 * @param http the HttpSecurity object to configure
+	 * @throws Exception if an error occurs while configuring the HttpSecurity object
+	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 						.csrf().disable()
 						.authorizeRequests()
 						.antMatchers("/auth/**").permitAll()
-						.anyRequest()
-						.authenticated()
+						.anyRequest().authenticated()
 						.and()
 						.logout()
 						.logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
