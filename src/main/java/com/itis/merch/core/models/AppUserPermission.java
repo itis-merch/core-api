259645dcf6822cf127191029d94ae641092ed419
vitterre <bagatovbb@gmail.com>
2023-04-16 19:51:32 +0300

    docs(domain): add docs for models

--- src/main/java/com/itis/merch/core/models/AppUserPermission.java
+++ src/main/java/com/itis/merch/core/models/AppUserPermission.java
@@ -4,20 +4,44 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
+/**
+ * The {@code AppUserPermission} enum represents the different
+ * permissions available to users in the application. Each
+ * permission is represented by a constant value and has a
+ * corresponding string value that can be used to check for
+ * authorization.
+ */
 @AllArgsConstructor
 public enum AppUserPermission {
-  
+
   /*----- Categories -----*/
 
+  /**
+   * Permission to view categories.
+   */
   CATEGORIES_VIEW("categories::view"),
+
+  /**
+   * Permission to write (create, edit, or delete) categories.
+   */
   CATEGORIES_WRITE("categories::write"),
 
 
   /*----- Products -----*/
 
+  /**
+   * Permission to view products.
+   */
   PRODUCTS_VIEW("products::view"),
+
+  /**
+   * Permission to write (create, edit, or delete) products.
+   */
   PRODUCTS_WRITE("products::write");
 
+  /**
+   * The string value of the permission.
+   */
   @Getter
   private final String permission;
 
