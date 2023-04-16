2a3f1c9925e8179a6d82e2767398ba1109271514
Marat <iimarioii3@mail.ru>
2023-04-16 19:49:29 +0300

    feat: add validateToken to JWTUtillService

--- src/main/java/com/itis/merch/core/security/JWTUtillService.java
+++ src/main/java/com/itis/merch/core/security/JWTUtillService.java
@@ -50,4 +50,9 @@ public class JWTUtillService {
 				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
 				.signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
 	}
+
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
 }
