f90643793972379318893a103e98bbf79a472c7e
Marat <iimarioii3@mail.ru>
2023-04-16 19:00:17 +0300

    feat: add extractAllClaims method to JWTUtillService

--- src/main/java/com/itis/merch/core/security/JWTUtillService.java
+++ src/main/java/com/itis/merch/core/security/JWTUtillService.java
@@ -1,8 +1,15 @@
 package com.itis.merch.core.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 public class JWTUtillService {
 
+	public <T> T extractAllClaim(String token) {
+		return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+	}
 }
