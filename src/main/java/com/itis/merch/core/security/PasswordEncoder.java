9c74b2f96651450dd9561f6a52f352c070ab75f2
Marat <iimarioii3@mail.ru>
2023-04-16 20:21:07 +0300

    feat(docx): add javadoc to PasswordEncoder

--- src/main/java/com/itis/merch/core/security/PasswordEncoder.java
+++ src/main/java/com/itis/merch/core/security/PasswordEncoder.java
@@ -4,9 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/**
+ * This is a configuration class for setting up a BCryptPasswordEncoder bean.
+ * It provides a single method for creating a new BCryptPasswordEncoder instance.
+ */
+
 @Configuration
 public class PasswordEncoder {
 
+	/**
+	 * This method creates a new BCryptPasswordEncoder instance.
+	 *
+	 * @return A new instance of the BCryptPasswordEncoder class.
+	 */
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
