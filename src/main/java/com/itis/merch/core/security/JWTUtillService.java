ee0b533e322bc36364cac413b46be1476ab9f002
Marat <iimarioii3@mail.ru>
2023-04-16 19:08:37 +0300

    feat: add SECRET_KEY

--- src/main/java/com/itis/merch/core/security/JWTUtillService.java
+++ src/main/java/com/itis/merch/core/security/JWTUtillService.java
@@ -2,6 +2,7 @@ package com.itis.merch.core.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
@@ -9,7 +10,10 @@ import java.util.function.Function;
 @Service
 public class JWTUtillService {
 
-	public <T> T extractAllClaim(String token) {
+	@Value("${jwt.seсret}")
+	private String SECRET_KEY;
+
+	public Claims extractAllClaim(String token) {
 		return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
 	}
 }
