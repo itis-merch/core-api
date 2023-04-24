a6c1fada9328e2360d7c6ef8161efb8b0fb5ced3
Marat <iimarioii3@mail.ru>
2023-04-24 21:13:02 +0300

    feat: add atributes and constructor to Option

--- src/main/java/com/itis/merch/core/models/Option.java
+++ src/main/java/com/itis/merch/core/models/Option.java
@@ -3,9 +3,9 @@ package com.itis.merch.core.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor
 @Entity
@@ -14,5 +14,22 @@ import javax.persistence.Table;
 public class Option {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
 	private Integer id;
+
+	@Column(name = "name")
+	@NotBlank
+	@NotNull
+	private String name;
+
+	@Column(name = "properties")
+	@NotBlank
+	@NotNull
+	private String properties;
+
+	public Option(@NotNull @NotBlank String name, @NotNull @NotBlank String properties) {
+		this.name = name;
+		this.properties = properties;
+	}
 }
