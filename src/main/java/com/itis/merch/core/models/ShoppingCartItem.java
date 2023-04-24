c146329e7b217a5cbaa415dd72277c99597413f7
Marat <iimarioii3@mail.ru>
2023-04-24 21:24:49 +0300

    feat: add anributes and constructor to ShoppingCartItem

--- src/main/java/com/itis/merch/core/models/ShoppingCartItem.java
+++ src/main/java/com/itis/merch/core/models/ShoppingCartItem.java
@@ -3,9 +3,9 @@ package com.itis.merch.core.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor
 @Entity
@@ -14,5 +14,26 @@ import javax.persistence.Table;
 public class ShoppingCartItem {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
 	private Integer id;
+
+	@OneToOne(mappedBy = "shopping_cart_item", cascade = CascadeType.ALL, orphanRemoval = true )
+	@NotNull
+	private Product product;
+
+	@OneToOne(mappedBy = "shopping_cart_item", cascade = CascadeType.ALL, orphanRemoval = true )
+	@NotNull
+	private Option option;
+
+	@Column(name = "quantity")
+	@NotNull
+	@NotBlank
+	private Integer quantity;
+
+	public ShoppingCartItem(@NotNull final Product product, @NotNull final Option option, @NotNull @NotBlank Integer quantity) {
+		this.product = product;
+		this.option = option;
+		this.quantity = quantity;
+	}
 }
