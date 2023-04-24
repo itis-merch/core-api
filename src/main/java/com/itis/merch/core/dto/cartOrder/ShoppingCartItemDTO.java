8bd7008db65117347f4ffbbf3dccc3887ae9797a
Marat <iimarioii3@mail.ru>
2023-04-24 22:21:03 +0300

    feat(docx): add javadoc to ShoppingCartItemDTO

--- src/main/java/com/itis/merch/core/dto/cartOrder/ShoppingCartItemDTO.java
+++ src/main/java/com/itis/merch/core/dto/cartOrder/ShoppingCartItemDTO.java
@@ -5,30 +5,54 @@ import com.itis.merch.core.models.ShoppingCartItem;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+/**
+ * Represents a data transfer object for a shopping cart item, including the unique identifier for the item,
+ * the ID of the associated product, the ID of the associated option, and the quantity of the product in the item.
+ * This class provides methods for creating instances of the class with the given field values.
+ */
 @Data
 @NoArgsConstructor
 public class ShoppingCartItemDTO {
 
+	/**
+	 * The unique identifier for the shopping cart item.
+	 */
 	@JsonProperty("id")
 	private Integer id;
 
+	/**
+	 * The ID of the associated product.
+	 */
 	@NotNull
 	@JsonProperty("product_id")
 	private Integer productId;
 
+	/**
+	 * The ID of the associated option.
+	 */
 	@NotNull
 	@JsonProperty("option_id")
 	private Integer optionId;
 
+	/**
+	 * The quantity of the product in the shopping cart item.
+	 */
 	@NotNull
 	@NotBlank
 	@JsonProperty("quantity")
 	private Integer quantity;
 
+	/**
+	 * Creates a new instance of the {@code ShoppingCartItemDTO} class with the given field values.
+	 *
+	 * @param id The unique identifier for the shopping cart item.
+	 * @param productId The ID of the associated product.
+	 * @param optionId The ID of the associated option.
+	 * @param quantity The quantity of the product in the shopping cart item.
+	 */
 	public ShoppingCartItemDTO(final Integer id, @NotBlank @NotNull Integer productId,
 							   @NotBlank @NotNull Integer optionId, @NotBlank @NotNull Integer quantity) {
 		this.id = id;
@@ -37,6 +61,12 @@ public class ShoppingCartItemDTO {
 		this.quantity = quantity;
 	}
 
+	/**
+	 * Creates a new instance of the {@code ShoppingCartItemDTO} class with the field values from the given
+	 * {@code ShoppingCartItem} instance.
+	 *
+	 * @param shoppingCartItem The {@code ShoppingCartItem} instance to create the DTO from.
+	 */
 	public ShoppingCartItemDTO(@NotNull final ShoppingCartItem shoppingCartItem) {
 		this.setId(shoppingCartItem.getId());
 		this.setProductId(shoppingCartItem.getProduct().getId());
