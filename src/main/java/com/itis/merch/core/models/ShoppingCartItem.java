8ad4c7dfb1cc80659566554d0ec45979f1372529
Marat <iimarioii3@mail.ru>
2023-04-24 20:58:01 +0300

    feat: add annotation and id to ShoppingCartItem

--- src/main/java/com/itis/merch/core/models/ShoppingCartItem.java
+++ src/main/java/com/itis/merch/core/models/ShoppingCartItem.java
@@ -1,4 +1,18 @@
 package com.itis.merch.core.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "shopping_cart_item")
+@Data
 public class ShoppingCartItem {
+
+	@Id
+	private Integer id;
 }
