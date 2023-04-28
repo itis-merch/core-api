d4a50b53fed430bc20cfe3dd2fdd1ac589107caf
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-04-29 02:50:58 +0300

    feat: add method (addItem) in CartOrderService

--- src/main/java/com/itis/merch/core/services/CartOrderService.java
+++ src/main/java/com/itis/merch/core/services/CartOrderService.java
@@ -28,4 +28,23 @@ public class CartOrderService {
 
 	private final AppUserRepository appUserRepository;
 
+	public void addItem(ShoppingCartItemDTO shoppingCartItemDTO, String userEmail) {
+		AppUser user = appUserRepository.findByEmailAddress(userEmail).get();
+		CartOrder cartOrder = cartOrderRepository.findCartOrderByUserId(user.getId())
+						.orElse(cartOrderRepository.save(new CartOrder(
+										user.getId(),
+										new ArrayList<ShoppingCartItem>(),
+										0,
+										user.getPhoneNumber(),
+										CartOrderStatus.CART)));
+		Product product = productRepository.getById(shoppingCartItemDTO.getProductId());
+		ShoppingCartItem shoppingCartItem = shoppingCartItemRepository.save(new ShoppingCartItem(
+						product,
+						optionRepository.getById(shoppingCartItemDTO.getOptionId()),
+						shoppingCartItemDTO.getQuantity()));
+		cartOrder.getShoppingCartItems().add(shoppingCartItem);
+		cartOrder.setTotalPrice(cartOrder.getTotalPrice()+shoppingCartItem.getQuantity()*product.getPrice());
+		cartOrderRepository.save(cartOrder);
+	}
+
 }
