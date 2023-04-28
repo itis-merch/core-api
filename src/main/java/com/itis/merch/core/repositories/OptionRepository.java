4f49d9d856de29e20cb8e8dff53d5295c6bbee06
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-04-29 02:54:31 +0300

    feat: create OptionRepository

---
+++ src/main/java/com/itis/merch/core/repositories/OptionRepository.java
@@ -0,0 +1,8 @@
+package com.itis.merch.core.repositories;
+
+import com.itis.merch.core.models.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionRepository extends JpaRepository<Option,Integer> {
+
+}
