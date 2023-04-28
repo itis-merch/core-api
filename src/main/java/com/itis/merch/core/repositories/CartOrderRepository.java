331c586ffbc16b1627d6b752f2fece24318b71ba
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-04-29 02:53:37 +0300

    feat: create CartOrderRepository

---
+++ src/main/java/com/itis/merch/core/repositories/CartOrderRepository.java
@@ -0,0 +1,10 @@
+package com.itis.merch.core.repositories;
+
+import com.itis.merch.core.models.CartOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartOrderRepository extends JpaRepository<CartOrder, Integer> {
+	Optional<CartOrder> findCartOrderByUserId(Integer userId);
+}
