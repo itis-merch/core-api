4470196df59af687075df304ea097c60a2abef2d
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-04-29 02:50:17 +0300

    feat: add fields for CartOrderService

--- src/main/java/com/itis/merch/core/services/CartOrderService.java
+++ src/main/java/com/itis/merch/core/services/CartOrderService.java
@@ -1,1 +1,31 @@
+package com.itis.merch.core.services;
 
+import com.itis.merch.core.dto.cartOrder.ShoppingCartItemDTO;
+import com.itis.merch.core.enums.CartOrderStatus;
+import com.itis.merch.core.exceptions.CustomException;
+import com.itis.merch.core.models.AppUser;
+import com.itis.merch.core.models.CartOrder;
+import com.itis.merch.core.models.Product;
+import com.itis.merch.core.models.ShoppingCartItem;
+import com.itis.merch.core.repositories.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class CartOrderService {
+
+	private final CartOrderRepository cartOrderRepository;
+
+	private final ShoppingCartItemRepository shoppingCartItemRepository;
+
+	private final ProductRepository productRepository;
+
+	private final OptionRepository optionRepository;
+
+	private final AppUserRepository appUserRepository;
+
+}
