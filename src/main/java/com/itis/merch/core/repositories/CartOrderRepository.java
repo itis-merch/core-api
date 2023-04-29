76196a3cbca3264406acdc0fa17e9821b9289760
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-04-29 03:01:20 +0300

    style: trivial changes

--- src/main/java/com/itis/merch/core/repositories/CartOrderRepository.java
+++ src/main/java/com/itis/merch/core/repositories/CartOrderRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface CartOrderRepository extends JpaRepository<CartOrder, Integer> {
 	Optional<CartOrder> findCartOrderByUserId(Integer userId);
+
 }
--- src/main/java/com/itis/merch/core/repositories/OptionRepository.java
+++ src/main/java/com/itis/merch/core/repositories/OptionRepository.java
@@ -3,6 +3,6 @@ package com.itis.merch.core.repositories;
 import com.itis.merch.core.models.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OptionRepository extends JpaRepository<Option,Integer> {
+public interface OptionRepository extends JpaRepository<Option, Integer> {
 
 }
