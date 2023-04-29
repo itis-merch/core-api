d71c2a0edc9e0bd8c246989522933bdbb5246145
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-04-29 03:19:27 +0300

    feat(docx): add javadoc toCartOrderRepository

--- src/main/java/com/itis/merch/core/repositories/CartOrderRepository.java
+++ src/main/java/com/itis/merch/core/repositories/CartOrderRepository.java
@@ -1,3 +1,7 @@
+/**
+
+ This interface represents the repository for the CartOrder entity.
+ */
 package com.itis.merch.core.repositories;
 
 import com.itis.merch.core.models.CartOrder;
@@ -6,6 +10,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CartOrderRepository extends JpaRepository<CartOrder, Integer> {
+	/**
+	 * Finds a CartOrder object based on the given user id.
+	 *
+	 * @param userId The user id for which the cart order is being searched.
+	 * @return Optional containing the CartOrder object if it exists, otherwise empty.
+	 */
 	Optional<CartOrder> findCartOrderByUserId(Integer userId);
 
 }
