653914c8516ae14e66ca5a8ee8e60de36b230209
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-04-29 03:22:33 +0300

    feat(docx): add javadoc to ShoppingCartItemRepository

--- src/main/java/com/itis/merch/core/repositories/ShoppingCartItemRepository.java
+++ src/main/java/com/itis/merch/core/repositories/ShoppingCartItemRepository.java
@@ -3,6 +3,10 @@ package com.itis.merch.core.repositories;
 import com.itis.merch.core.models.ShoppingCartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for ShoppingCartItem model.
+ */
 public interface ShoppingCartItemRepository extends JpaRepository<ShoppingCartItem, Integer> {
 
 }
+
