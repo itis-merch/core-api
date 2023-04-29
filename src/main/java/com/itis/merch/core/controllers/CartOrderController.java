60d4e2a2d4f5b5a5bcd322033e4cf0783aecce6b
JouCRB <112938471+JouCRB@users.noreply.github.com>
2023-04-29 03:12:13 +0300

    feat(docx): add javadoc to CartOrderController

--- src/main/java/com/itis/merch/core/controllers/CartOrderController.java
+++ src/main/java/com/itis/merch/core/controllers/CartOrderController.java
@@ -1,3 +1,7 @@
+/**
+
+ This class represents the controller for handling requests related to the shopping cart and orders.
+ */
 package com.itis.merch.core.controllers;
 
 import com.itis.merch.core.common.ApiResponse;
@@ -6,7 +10,6 @@ import com.itis.merch.core.exceptions.CustomException;
 import com.itis.merch.core.security.JWTUtilService;
 import com.itis.merch.core.services.CartOrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cart")
 @RequiredArgsConstructor
 public class CartOrderController {
-
 	private final CartOrderService cartOrderService;
 
 	private final JWTUtilService jwtUtilService;
 
+	/**
+	 * This method handles the request for adding an item to the shopping cart.
+	 *
+	 * @param shoppingCartItemDTO the DTO object containing the details of the item to be added.
+	 * @param authentication      the object representing the authentication of the user making the request.
+	 * @return ResponseEntity      the response containing the success status and message.
+	 */
 	@PostMapping("/add")
 	public ResponseEntity<ApiResponse> addCartItem(@RequestBody final ShoppingCartItemDTO shoppingCartItemDTO, Authentication authentication) {
 
@@ -30,12 +39,17 @@ public class CartOrderController {
 
 		cartOrderService.addItem(shoppingCartItemDTO, userEmail);
 
-		return new ResponseEntity<>(
-						new ApiResponse(true, "Product was added to cart successfully."),
-						HttpStatus.CREATED
-		);
+		return ResponseEntity.ok()
+						.body(new ApiResponse(true, "Product was added to cart successfully."));
 	}
 
+	/**
+	 * This method handles the request for placing an order based on the contents of the shopping cart.
+	 *
+	 * @param authentication the object representing the authentication of the user making the request.
+	 * @return ResponseEntity      the response containing the success status and message.
+	 * @throws CustomException if there is an error while processing the order.
+	 */
 	@PostMapping("/order")
 	public ResponseEntity<ApiResponse> order(Authentication authentication) throws CustomException {
 
@@ -43,11 +57,8 @@ public class CartOrderController {
 
 		cartOrderService.order(userEmail);
 
-		return new ResponseEntity<>(
-						new ApiResponse(true, "Product was added to cart successfully."),
-						HttpStatus.CREATED
-		);
-
+		return ResponseEntity.ok()
+						.body(new ApiResponse(true, "Order was placed successfully."));
 	}
 
 }
